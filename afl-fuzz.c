9b6d8207b76a93719845e56ef93a9bab5aa3badd
yihuan18 <18200120142@163.com>
2018-11-13 15:17:27 +0800

    修改ip为本地环回地址

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -341,11 +341,7 @@ int createSocket()
   memset(&mysockaddr, 0x00, sizeof(mysockaddr));
   mysockaddr.sin_family = AF_INET;
   mysockaddr.sin_port = htons(PORT);
-<<<<<<< HEAD
-  mysockaddr.sin_addr.s_addr = inet_addr("192.168.1.234");
-=======
-  mysockaddr.sin_addr.s_addr = inet_addr("192.168.1.34");
->>>>>>> 71cb43f8222b623b980a496f1a270ff88b083b52
+  mysockaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
   if (-1 == connect(csocfd, (struct sockaddr *)&mysockaddr, sizeof(mysockaddr)))
   {
